8eb7ca5a16bc3c4b8fe24eda407ec2cac5e801b4
takahiro-hashimoto <bridge.takahiro.1539@gmail.com>
2017-03-17 10:28:32 +0900

    Update script.js

--- script/script.js
+++ script/script.js
@@ -3,7 +3,6 @@ $(function(){
   var $inputTask = $('#js-input-task');
   var $itemList = $('#js-itemList');
 
-  //入力欄にフォーカス
   $inputTask.focus();
 
   //追加機能
@@ -22,29 +21,27 @@ $(function(){
   //削除機能
   $(document).on('click', '.delete', function(){
     var id=$(this).parent().data('id');
-    $.post('delete.php',
-     {
-      id:id
-     },function(num){
-       $('#js-itemList-' + id).remove();
+    $.post('delete.php', {id:id}, function(num){
+      $('#js-itemList-' + id).remove();
     });
   });
 
   //編集機能
   $(document).on('click', '.edit', function(){
-  　　var id= $(this).parent().data('id');
-  　　var title = $(this).prev().text();
-  　　$('#js-itemList-' + id).empty().append($('<input class="edit-input task-input" type="text">').attr('value', title)).append('<button class="update ui button">更新</button>');
-  　　$('#js-itemList-' + id + 'input:eq(0)').focus();
+  　var id= $(this).parent().data('id');
+  　var title = $(this).prev().text();
+  　$('#js-itemList-' + id).empty().append($('<input class="edit-input task-input" type="text">').attr('value', title)).append('<button class="update ui button">更新</button>');
+  　$('#js-itemList-' + id + 'input:eq(0)').focus();
   });
 
   //更新機能
   $(document).on('click', '.update', function(){
-  var id = $(this).parent().data('id');
-  var title = $(this).prev().val();
-   $.post('update.php',{id:id, title:title},function(num){
-   var buttonStr = $('<span></span><button class="edit ui button">編集</button><button class="delete ui button">完了</button>');
-        $('#js-itemList-' + id).empty().append(buttonStr).find('span:eq(0)').text(title);
-     });
+    var id = $(this).parent().data('id');
+    var title = $(this).prev().val();
+    $.post('update.php', {id:id, title:title}, function(num){
+      var buttonStr = $('<span></span><button class="edit ui button">編集</button><button class="delete ui button">完了</button>');
+      $('#js-itemList-' + id).empty().append(buttonStr).find('span:eq(0)').text(title);
     });
+  });
+  
 });
