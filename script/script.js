20e4dc5dbfb5789777655a4422049b37c6d3979e
takahiro-hashimoto <bridge.takahiro.1539@gmail.com>
2017-03-17 11:33:14 +0900

    Update script.js

--- script/script.js
+++ script/script.js
@@ -11,19 +11,34 @@ $(function(){
       return false;
     } else {
       var title = $("#js-input-task").val();
-      $.post('add.php', {title:title}, function(num){
-        var itemStr = $('<div id="js-itemList-' + num + '"data-id="' + num + '" class="ui segment itemList-item"><span>title</span><button class="edit ui button">編集</button><button class="delete ui button">完了</button></div>');
-        $inputTask.val('').focus();
-      });
+      $.post(
+        //PHPファイルの指定
+        'add.php',
+        {
+          //POSTで渡す値の配列
+          title: title
+        },
+        //返り値を取得しての実行
+        function(num) {
+          var itemStr = $('<div id="js-itemList-' + num + '"data-id="' + num + '" class="ui segment itemList-item"><span>title</span><button class="edit ui button">編集</button><button class="delete ui button">完了</button></div>');
+          $inputTask.val('').focus();
+        }
+      );
     }
   });
 
   //削除機能
   $(document).on('click', '.delete', function(){
     var id=$(this).parent().data('id');
-    $.post('delete.php', {id:id}, function(num){
-      $('#js-itemList-' + id).remove();
-    });
+    $.post(
+      'delete.php', 
+      {
+        id: id
+      }, 
+      function(num){
+        $('#js-itemList-' + id).remove();
+      }
+    );
   });
 
   //編集機能
@@ -38,10 +53,16 @@ $(function(){
   $(document).on('click', '.update', function(){
     var id = $(this).parent().data('id');
     var title = $(this).prev().val();
-    $.post('update.php', {id:id, title:title}, function(num){
-      var buttonStr = $('<span></span><button class="edit ui button">編集</button><button class="delete ui button">完了</button>');
-      $('#js-itemList-' + id).empty().append(buttonStr).find('span:eq(0)').text(title);
-    });
+    $.post(
+      'update.php',
+      {
+        id: id, title: title
+      }, 
+      function(num){
+        var buttonStr = $('<span></span><button class="edit ui button">編集</button><button class="delete ui button">完了</button>');
+        $('#js-itemList-' + id).empty().append(buttonStr).find('span:eq(0)').text(title);
+      }
+    );
   });
-  
+
 });
